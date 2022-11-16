0bce7431525beb27a1254e3054bc0180660b7280
phucthuan1st <phucthuan1st@users.noreply.github.com>
2022-11-17 00:32:31 +0700

    update data test

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
-g++ allRun.cpp Brute-force.cpp KMP.cpp Rabin-Karp.cpp -o allRun ; ./allRun
+g++ allRun.cpp Brute-force.cpp KMP.cpp Rabin-Karp.cpp -o allRun ; ./allRun;
+git add .
+git commit -m "update data test"
+gi push
